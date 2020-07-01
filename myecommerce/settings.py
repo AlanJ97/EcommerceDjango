4615649ec242d8455b77844ab7a631db1e65db85
Alan <alannavaseg_02@hotmail.com>
2020-07-01 13:02:45 -0500

    Se agregan las direcciones locales con static de djnago, algunas no fueron reconozidas, pero esperamos sea algo normal

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -68,7 +68,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files'), 
+ '/var/www/static/',
+'/home/alan/Documents/Python/Ecommerce/django_project_boilerplate/static_in_env',
+ ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
